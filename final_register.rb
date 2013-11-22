32db60f46bd821806a78bac9e234f42b6082cc5b
Lorry Rocha <sorai120@yahoo.com>
2013-11-22 14:02:58 -0500

    We now have a fully functioning register, yaaaaay

--- final_register.rb
+++ final_register.rb
@@ -12,8 +12,6 @@ def format_string_to_sym(word)
   word.intern
 end
 
-
-
 def capitalize(name)
   names = []
   name.split(' ').each do |word|
@@ -57,7 +55,11 @@ end
 def select_option
   puts "Make a selection:"
   @selection = gets_input
-  select_option unless valid_selection?
+  if check_done?
+    true
+  elsif !valid_selection?
+    select_option
+  end
 end
 
 def select_amount_of_bags
@@ -69,18 +71,63 @@ end
 def display_subtotal
     @total += ((@item_list[@selection.to_i][:purchasing_price]).to_f*@amount.to_i)
     puts "Subtotal: $#{format_number(@total)}"
+    @time = Time.new
+end
+# Time.new.strftime("%Y-%m-%d")] = time:(Time.new.strftime("%H:%M:%S"
+def update_shopping_list
+  temp_hash = {}
+  temp_hash[:total_number_items] = @amount
+  temp_hash[:sku] = @selection
+  temp_hash[:item_total] = (@item_list[@selection.to_i][:purchasing_price]).to_f*@amount.to_i
+  temp_hash[:name] = @item_list[@selection.to_i][:item]
+  @shopping_list[@order_number] = temp_hash
+  @order_number += 1
 end
 
-def update_shopping_list
-  @shopping_list[@order_number] = {}
-  @shopping_list[@order_number] = {date:(Time.new.strftime("%Y-%m-%d")),time:(Time.new.strftime("%H:%M:%S"))}
-  @order_number +=1
+def display_complete_sale
+  puts "===Sale Complete===\n\n\n"
+  @shopping_list.each do |key, value|
+    puts "$#{format_number(@shopping_list[key][:total_number_items].to_f * @shopping_list[key][:item_total].to_f)} - #{@shopping_list[key][:total_number_items].to_i} #{capitalize(@shopping_list[key][:name])}"
+  end
+  puts "\n\nTotal: $#{format_number(@total)}"
+end
+
+def recursive_gets
+  puts "What is the amount tendered?"
+  @input = gets_input
+  recursive_gets unless valid_input?
+end
+
+def checkout
+  recursive_gets
+  calc_change
+  if  @input.to_f >= @total
+    successful_checkout
+  else
+    unsuccessful_checkout
+  end
+end
+
+def calc_change
+  (@total - @input.to_f).abs
+end
+
+def unsuccessful_checkout
+  puts "WARNING: The customer still owes $#{format_number(calc_change)}! Exiting..."
+end
+
+def successful_checkout
+  puts "===Thank You!==="
+  print "The total change due is $#{format_number(calc_change)}\n\n"
+  puts @time.strftime("%m-%d-%Y %H:%M:%S")
+  puts "================"
 end
 
 #The stuff above are methods for basic register functionality
 
 def check_done?
-  @input.downsize == "done"
+  @input.downcase! unless valid_numeric?
+  @input == "done"
 end
 
 def valid_selection?
@@ -100,7 +147,12 @@ end
 populate_item_list('item_list.csv',@item_list)
 display_item_menu(@item_list)
 select_option
-select_amount_of_bags
-display_subtotal
-binding.pry
+until check_done?
+  select_amount_of_bags
+  display_subtotal
+  update_shopping_list
+  select_option
+end
+display_complete_sale
+checkout
 
